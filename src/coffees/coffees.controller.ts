f71aa80a6731f9eec30b35f214b89083c6019b41
Pasindu Manaranjana <algopasindu@gmail.com>
2025-10-07 14:27:18 +0530

    patch and delete basic setup

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -8,12 +16,22 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id) {
+  findOne(@Param('id') id: string) {
     return `This returns coffee id: ${id}`;
   }
 
   @Post()
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id') // ':id' Indicates what coffee we are going to update.
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
