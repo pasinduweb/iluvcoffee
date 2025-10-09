afbe4c5dbb86445b1aabb3d937d2e20950b64836
Pasindu Manaranjana <algopasindu@gmail.com>
2025-10-09 13:55:19 +0530

    Create coffee and Update coffee DTOs

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -9,6 +9,8 @@ import {
   Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -26,13 +28,13 @@ export class CoffeesController {
   }
 
   @Post()
-  create(@Body() body) {
-    return this.CoffeeService.create(body);
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.CoffeeService.create(createCoffeeDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return this.CoffeeService.update(id, body);
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.CoffeeService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
