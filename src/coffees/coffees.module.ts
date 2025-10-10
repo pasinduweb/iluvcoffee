c0a0bd226f56f8ba3060440c9537f91c65caa24c
Pasindu Manaranjana <algopasindu@gmail.com>
2025-10-10 22:53:06 +0530

    factory providers demo

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,26 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
-class ConfigService {}
-class DevConfigService {}
-class ProdConfigService {}
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    // Do something
+    return ['brew', 'nescafe'];
+  }
+}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    CoffeeBrandsFactory,
     {
-      provide: ConfigService,
-      useClass:
-        process.env.NODE_ENV === 'development'
-          ? DevConfigService
-          : ProdConfigService,
+      provide: COFFEE_BRANDS,
+      useFactory: (brandFactory: CoffeeBrandsFactory) => brandFactory.create(),
+      inject: [CoffeeBrandsFactory],
     },
   ],
   exports: [CoffeesService],
