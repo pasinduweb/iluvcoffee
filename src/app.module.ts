a1801de766a2709cd0844dbb617bc23fb3f6a5c6
Pasindu Manaranjana <algopasindu@gmail.com>
2025-10-11 15:20:43 +0530

    validate env configs schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5433),
+      }),
     }),
     CoffeesModule,
     TypeOrmModule.forRoot({
